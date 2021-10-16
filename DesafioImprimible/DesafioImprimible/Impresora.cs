8f9e25d4c3369faefa631930091e764495553edb
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2021-10-16 18:52:25 -0300

    imprimible

---
+++ DesafioImprimible/DesafioImprimible/Impresora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioImprimible
+{
+    class Impresora
+    {
+        List<Iimprimible> colaImpresion = new List<Iimprimible>();
+
+        public void ImprimirTodo()
+        {
+            foreach(Iimprimible imp in colaImpresion)
+            {
+                imp.imprimir();
+            }
+        }
+
+        public void AgregarImprimible(Iimprimible imp)
+        {
+            colaImpresion.Add(imp);
+        }
+
+    }
+}
---
+++ DesafioImprimible/DesafioImprimible/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+ *  Laboratorio II
+ *  Prof. Jesús Arce
+ *  
+ *  Desafío de interfaces
+ *  
+ *  Autor: Lic. Francisco Javier González del Solar
+ * 
+ */
+
+namespace DesafioImprimible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contrato contrato = new Contrato();
+            Foto foto = new Foto();
+            Documento doc = new Documento();
+
+            Impresora impresora = new Impresora();
+
+            impresora.AgregarImprimible(contrato);
+            impresora.AgregarImprimible(foto);
+            impresora.AgregarImprimible(doc);
+
+            impresora.ImprimirTodo();
+        }
+    }
+}
